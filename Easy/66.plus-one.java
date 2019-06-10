057c0ac25880baed6f114d5ebfb75035e4f57cc8
aptx4869hrj <1571170555@qq.com>
2019-06-10 14:16:14 +0800

    update

--- Easy/66.plus-one.java
+++ Easy/66.plus-one.java
@@ -5,7 +5,21 @@
  */
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+        for(int i = digits.length - 1; i >= 0;i--){
+            if(digits[i] < 9){ 
+                digits[i]++;
+                return digits; 
+            }else{
+                digits[i] = 0;
+            }
+        }
+
+        if(digits[0] == 0){
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
+        }
+        return digits;
     }
 }
 
