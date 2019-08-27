4056cd4501bd935d5d83d3ef9fd3b81bc0a125fa
aptx4869hrj <1571170555@qq.com>
2019-08-28 00:08:24 +0800

    update

---
+++ Medium/80.remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=80 lang=java
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if(count < 2 || nums[count-2] > num){
+                nums[count] = num;
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
