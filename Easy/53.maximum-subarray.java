2ec5b37ce220c0607b6074f623b9e62a0dc5c266
aptx4869hrj <1571170555@qq.com>
2019-09-26 15:45:16 +0800

    update

--- Easy/53.maximum-subarray.java
+++ Easy/53.maximum-subarray.java
@@ -5,7 +5,13 @@
  */
 class Solution {
     public int maxSubArray(int[] nums) {
-        
+        int curmax = 0;
+        int result = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            curmax = Math.max(nums[i], curmax + nums[i]);
+            result = Math.max(curmax, result);
+        }
+        return result;
     }
 }
 
